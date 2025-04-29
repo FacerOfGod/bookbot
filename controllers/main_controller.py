54d44698bfa6120ae388aae34ec63d665700688b
FacerOfGod <spichot2@gmail.com>
2025-04-29 13:34:18 +0200

    Added a upload button that is semi functional

---
+++ controllers/main_controller.py
@@ -0,0 +1,22 @@
+import os
+
+from PySide6.QtWidgets import QFileDialog
+
+class MainController:
+    def __init__(self, view):
+        self.view = view
+
+    def the_button_was_clicked(self):
+        self.button.setText("You clicked me!")
+        self.button.setEnabled(False)
+        self.setWindowTitle("Button Clicked!")
+
+    def upload_document(self) -> str:
+        file_path, _ = QFileDialog.getOpenFileName(None, "Choose file", "", "All Files (*)")
+        if file_path:
+            file_name = os.path.basename(file_path)
+        else:
+            file_name = "Error File"
+        self.view.update_buttonUpload_text(file_name)
+        return file_name
+    
--- views/main_window.py
+++ views/main_window.py
@@ -1,10 +1,17 @@
+import os
+
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import *
 from PySide6.QtGui import QFont
+from controllers.main_controller import MainController
 
+#TODO figure a better way to implement styles
 class MainWindow(QMainWindow):
+    file_path = None
+
     def __init__(self):
         super().__init__()
+        self.controller = MainController(self)
 
         self.setWindowTitle("Cool Interface")
         self.setMinimumSize(400, 300)
@@ -17,12 +24,31 @@ class MainWindow(QMainWindow):
         self.empty_box.setMinimumSize(200, 150)
         self.empty_box.setStyleSheet("background-color: #f0f0f0; border: 2px solid #333; border-radius: 10px;")
 
+        self.buttonUpload = QPushButton("Upload file here")
+        self.buttonUpload.setFont(QFont("Arial", 12, QFont.Bold))
+        self.buttonUpload.setStyleSheet("""
+        QPushButton {
+                background-color: #2196F3;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                border-radius: 5px;
+                min-width: 120px;
+            }
+            QPushButton:hover {
+                background-color: #1976D2;
+            }
+            QPushButton:pressed {
+                background-color: #1565C0;
+            }
+        """)
+        self.buttonUpload.clicked.connect(self.controller.upload_document)
+
+
         self.button = QPushButton("Click Me!")
         self.button.setFont(QFont("Arial", 12, QFont.Bold))
-
-        #TODO figure a better way to implement styles
         self.button.setStyleSheet("""
-            QPushButton {
+        QPushButton {
                 background-color: #4CAF50;
                 color: white;
                 border: none;
@@ -37,12 +63,12 @@ class MainWindow(QMainWindow):
                 background-color: #3d8b40;
             }
         """)
-        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.controller.the_button_was_clicked)
 
         layout.addWidget(self.empty_box, alignment=Qt.AlignCenter)
+        layout.addWidget(self.buttonUpload, alignment=Qt.AlignCenter)
         layout.addWidget(self.button, alignment=Qt.AlignCenter)
+
+    def update_buttonUpload_text(self, new_text):
+        self.buttonUpload.setText(new_text)
     
-    def the_button_was_clicked(self):
-        self.button.setText("You clicked me!")
-        self.button.setEnabled(False)
-        self.setWindowTitle("Button Clicked!")
