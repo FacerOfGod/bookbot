e7c5cf5551d01663e746110ab6ca012c4148b78b
FacerOfGod <spichot2@gmail.com>
2025-05-02 18:48:52 +0200

    Added test for wiki button

--- tests/test_main_window.py
+++ tests/test_main_window.py
@@ -16,6 +16,15 @@ def test_initial_state(main_window):
     assert main_window.buttonUpload.text() == "Upload file here"
     assert not main_window.buttonDelete.isVisible()
 
+def test_wiki_button(main_window, ):
+    """Check Wiki button state."""
+    assert main_window.file_path is None
+    assert main_window.button.text() == "Wiki search"
+    assert not main_window.button.isEnabled()
+    main_window.input_box.setText("Test")
+    assert main_window.button.isEnabled()
+
+
 def test_set_file_path(main_window, tmp_path):
     """Test that setting a file path updates the GUI."""
     dummy_file = tmp_path / "dummy.txt"
